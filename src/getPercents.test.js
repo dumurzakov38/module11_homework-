518c8e1389599108e20b3d9ba50f426feedb9734
dumurzakov38 <118802258+dumurzakov38@users.noreply.github.com>
2023-08-18 10:28:12 +0000

    git commit -m "  "

--- src/getPercents.test.js
+++ src/getPercents.test.js
@@ -1,21 +1,33 @@
 const getPercents = require('./getPercents');
 
 describe('getPercents', () => {
+  const percent10 = 10;
+  const percent20 = 20;
+  const percent25 = 25;
+  const percent80 = 80;
+  const percent200 = 200;
+
+  const percent0 = 0;
+  const percent50 = 50;
+  const percent150 = 150;
+  const percent5000 = 5000;
+  const percent10000 = 10000;
+
   it('should calculate correct percentages', () => {
-    expect(getPercents(10, 200)).toBe(20);
-    expect(getPercents(25, 80)).toBe(20);
+    expect(getPercents(percent10, percent200)).toBe(percent20);
+    expect(getPercents(percent25, percent80)).toBe(percent20);
   }),
 
   it('should throw an error for non-number arguments', () => {
-    expect(() => getPercents("10", 200)).toThrow('Both arguments should be numbers');
-    expect(() => getPercents(10, "200")).toThrow('Both arguments should be numbers');
+    expect(() => getPercents("10", percent200)).toThrow('Both arguments should be numbers');
+    expect(() => getPercents(percent10, "200")).toThrow('Both arguments should be numbers');
     expect(() => getPercents("10", "200")).toThrow('Both arguments should be numbers');
   }),
 
   it('should handle corner cases', () => {
-    expect(getPercents(0, 150)).toBe(0);
-    expect(getPercents(50, 0)).toBe(0);
-    expect(getPercents(10000, 50)).toBe(5000);
+    expect(getPercents(percent0, percent150)).toBe(percent0);
+    expect(getPercents(percent50, percent0)).toBe(percent0);
+    expect(getPercents(percent10000, percent50)).toBe(percent5000);
   });
 });
 
